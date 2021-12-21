fdca8ed16fdb0f4cc84be4ac9c6709c6d9e3e4a3
Chad Stark <chad.d.stark@hotmail.com>
2021-12-21 08:48:48 -0800

    implementing login component. no functionality atm.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import RenderLogin from "./pages/login";
 
 function App() {
   return (
-    <div className="bg-blue-400 ">
-      <div className="grid grid-cols-6 w-full">
-        <div className=" ">1</div>
-        <div className="col-span-4 bg-violet-300 min-h-screen">
-          <RenderLogin />
-        </div>
-        <div className="">3</div>
+    <div className="bg-zinc-500 flex justify-center min-h-screen">
+      <div className="flex items-center">
+        <RenderLogin />
       </div>
     </div>
   );
