2bd0bb92657e0bd230cd07ea16181d355e875e90
Chad Stark <chad.d.stark@hotmail.com>
2022-01-15 17:53:38 -0800

    navigates to profile page when login button is clicked.

--- src/pages/login.js
+++ src/pages/login.js
@@ -21,6 +21,10 @@ function RenderLogin() {
     console.log(response);
     return navigate("/profile");
   };
+  const responseCredentialLogin = (response) => {
+    console.log(response);
+    return navigate("/profile");
+  };
   return (
     <div className="bg-zinc-500 flex justify-center min-h-screen">
       <div className="flex items-center">
@@ -104,6 +108,7 @@ function RenderLogin() {
                 <button
                   type="submit"
                   class="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                  onClick={responseCredentialLogin}
                 >
                   Login
                 </button>
